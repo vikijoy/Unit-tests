5e468a68191bd22d79dfc7fd7fb8fd0256521b18
Victoria Nikulkina <nikulkina93@list.ru>
2023-12-06 23:16:11 +0700

    hw4

---
+++ hw4/task2/tests.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest.mock import Mock
+
+from book import BookService
+
+
+class TestBookService(unittest.TestCase):
+    def setUp(self) -> None:
+        self.service = BookService(Mock())
+
+    def test_find_by_id(self):
+        book_id = 1
+        self.service.find_by_id(book_id)
+        self.service.book_repository.find_by_id.assert_called_with(book_id)
+
+    def test_find_all(self):
+        self.service.find_all()
+        self.service.book_repository.find_all.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
